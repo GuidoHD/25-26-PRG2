79b20b261b736d9a725b970eb1adce2234ceb0cc
Marcos <marcoshuidobrodiegorgmail.com>
2026-03-23 19:36:23 +0100

    métodos formados, mcd añadido

--- entregas/Fraccion.java
+++ entregas/Fraccion.java
@@ -19,34 +19,71 @@ class Fraccion{
 
         if (denominador < 0){
             numerador = numerador*-1;
-            denominador = -denominador*-1;
+            denominador = denominador*-1;
         }
 
-        int mcd = calcularMCD(numerador, denominador){
+        int mcd = calcularMCD(numerador, denominador);
             this.numerador = numerador/mcd;
             this.denominador = denominador/mcd;
-        }
+        
 
     }
 
 
-    public Fraccion sumar(Fraccion diferente)
-
-    public Fraccion restar(Fraccion diferente)
-
-    public Fraccion multiplicar(Fraccion diferente)
-
-    public Fraccion dividir(Fraccion diferente)
-
-
-    public Fraccion inversa()
-
-    public Fraccion opuesta()
+    private int calcularMCD(int a, int b){
+        while (b != 0) {
+        int temporal = b;
+        b = a % b;
+        a = temporal;
+    }
+    return a;
+    }
 
 
-    public boolean esIgual()
+    public Fraccion sumar(Fraccion secundaria){
+        int numerador = (this.numerador*secundaria.denominador)+(this.denominador*secundaria.numerador);
+        int denominador = (this.denominador*secundaria.denominador);
+        return new Fraccion(numerador, denominador);
+    }
 
-    public String toString()
+
+    public Fraccion restar(Fraccion secundaria){
+        int numerador = (this.numerador*secundaria.denominador)-(this.denominador*secundaria.numerador);
+        int denominador = (this.denominador*secundaria.denominador);
+        return new Fraccion(numerador, denominador);
+    }
+
+    public Fraccion multiplicar(Fraccion secundaria){
+        int numerador = this.numerador*secundaria.numerador;
+        int denominador = this.denominador*secundaria.denominador;
+        return new Fraccion(numerador,denominador);
+    }
+
+    public Fraccion dividir(Fraccion secundaria){
+        int numerador = this.numerador*secundaria.denominador;
+        int denominador = this.denominador*secundaria.numerador;
+        return new Fraccion(numerador,denominador);
+    }
+
+
+
+    public Fraccion opuesta(){
+        return new Fraccion(-this.numerador,this.denominador);
+    }
+
+    public Fraccion inversa(){
+        return new Fraccion(this.denominador,this.numerador);
+    }
+
+
+    public boolean esIgual(Fraccion secundaria){
+        return this.numerador == secundaria.numerador && this.denominador == secundaria.denominador;
+    }
+
+
+    public String toString(){
+        return numerador + "/" + denominador;
+    }
 
 
 }
