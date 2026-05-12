dc3c1583c4fff6db6e9ef0668b6d342cb9e30be3
Marcos <marcoshuidobrodiegorgmail.com>
2026-05-12 10:54:42 +0200

    m blackjack

--- entregas/huidobroMarcos/Blackjack.java
+++ entregas/huidobroMarcos/Blackjack.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Blackjack{
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int opcion = 0;
+
+         while (opcion != 3) {
+            Baraja baraja = new Baraja();
+            Mano mano = new Mano();
+
+            // Repartir dos cartas iniciales
+            mano.agregarCarta(baraja.sacarCarta());
+            mano.agregarCarta(baraja.sacarCarta());
+
+            boolean partidaTerminada = false;
+
+        }
+    }
 
     
 
