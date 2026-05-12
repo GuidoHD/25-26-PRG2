370d55719fe3d13eee52f65c897354893ab9ef37
Marcos <marcoshuidobrodiegorgmail.com>
2026-05-12 10:52:07 +0200

    mano

--- entregas/huidobroMarcos/Mano.java
+++ entregas/huidobroMarcos/Mano.java
@@ -11,5 +11,40 @@ class Mano {
     public void agregarCarta(Carta carta) {
         cartas.add(carta);
     }
+
+
+    public int calcularPuntos() {
+        int puntos = 0;
+        int ases = 0;
+
+        for (Carta carta : cartas) {
+            String valor = carta.getValor();
+
+            if (valor.equals("A")) {
+                puntos += 11;
+                ases++;
+            } else if (valor.equals("J") || valor.equals("Q") || valor.equals("K")) {
+                puntos += 10;
+            } else {
+                puntos += Integer.parseInt(valor);
+            }
+        }
+        while (puntos > 21 && ases > 0) {
+            puntos -= 10;
+            ases--;
+        }
+
+        return puntos;
+    }
+
+    public String toString() {
+        String resultado = "";
+
+        for (Carta carta : cartas) {
+            resultado += "[" + carta.toValorPalo() + "]";
+        }
+
+        return resultado;
+    }
     
 }
