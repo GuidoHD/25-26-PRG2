e24f15a2fd39f7ccd30e42b5ca4d3584038cc6ae
Marcos <marcoshuidobrodiegorgmail.com>
2026-05-12 10:50:15 +0200

    array mano

--- entregas/huidobroMarcos/Mano.java
+++ entregas/huidobroMarcos/Mano.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 class Mano {
+
+    private ArrayList<Carta> cartas;
+
+    public Mano() {
+        cartas = new ArrayList<>();
+    }
+
+    public void agregarCarta(Carta carta) {
+        cartas.add(carta);
+    }
     
 }
